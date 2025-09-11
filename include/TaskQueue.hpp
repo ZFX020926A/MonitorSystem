c4cc8bf6f6875584301b64e3f4ef663c0f730a23
fuxiang <fuxiang1777@gmail.com>
2025-09-11 19:31:07 +0800

    完成任务队列的封装

---
+++ include/TaskQueue.hpp
@@ -0,0 +1,41 @@
+#ifndef __TASKQUEUE_H__
+#define __TASKQUEUE_H__
+
+// 任务队列 的封装
+
+#include "MutexLock.hpp"
+#include "Condition.hpp"
+
+#include <queue>
+#include <functional>
+
+using std::queue;
+using std::function;
+
+using Element = function<void()>;
+
+class TaskQueue
+{
+public:
+    TaskQueue(int taskQueueSize);
+    ~TaskQueue();
+
+    bool full();
+    bool empty();
+    void push(Element &&ele);
+    Element pop();
+    void wakeup();
+    int getTaskQueueSize();
+private:
+    int _taskQueueSize;
+    MutexLock _mutex;
+    Condition _notFull;
+    Condition _notEmpty;
+    queue<Element> _taskQueue;
+    bool _flage;
+};
+
+
+
+
+#endif // !__TASKQUEUE_H__
---
+++ src/TaskQueue.cpp
@@ -0,0 +1,66 @@
+#include "TaskQueue.hpp"
+
+TaskQueue::TaskQueue(int taskQueueSize)
+: _taskQueueSize(taskQueueSize)
+, _mutex()
+, _notFull(_mutex)
+, _notEmpty(_mutex)
+, _flage(true)
+{
+}
+
+TaskQueue::~TaskQueue()
+{
+}
+
+bool TaskQueue::full()
+{
+    return _taskQueue.size() == _taskQueueSize;
+}
+
+bool TaskQueue::empty()
+{
+    return _taskQueue.size() == 0;;
+}
+
+void TaskQueue::push(Element &&ele)
+{
+    MutexLockGuard autolock(_mutex);
+    while(full())
+    {
+        _notFull.wait();
+    }
+
+    _taskQueue.push(std::move(ele));
+    _notEmpty.notify();
+}
+
+Element TaskQueue::pop()
+{
+    MutexLockGuard autolock(_mutex);
+    while(empty() && _flage)
+    {
+        _notEmpty.wait();
+    }
+
+    if(!_flage)
+    {
+        return nullptr;
+    }
+    Element ele = _taskQueue.front();
+    _taskQueue.pop();
+    _notFull.notify();
+
+    return ele;
+}
+
+void TaskQueue::wakeup()
+{
+    _flage = false;
+    _notEmpty.notifyAll();
+}
+
+int TaskQueue::getTaskQueueSize()
+{
+    return _taskQueueSize;
+}
